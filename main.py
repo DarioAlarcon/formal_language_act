d1cd1bb4b9d0a85a984bee9fea2cb7938c963cef
Dario Alarcon <darioalarcon832@gmail.com>
2023-09-07 16:35:15 -0500

    third commit, solutions of problems with 'cerradura de estrella' logic and implementation of the funtion to concatenate the languages

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def mezclar_elementos(entrada):
         elementos_aleatorios1 = random.choice(entrada_lista)
         c = 0
         lenn = len(iterador_set_list)
-        while c < lenn:
+        while c < lenn and len(nuevo_set) < 20:
             nuevo_elemento = elementos_aleatorios1[0] + iterador_set_list[c]
             if nuevo_elemento not in nuevo_set:
                 nuevo_set.add(nuevo_elemento)
@@ -18,6 +18,21 @@ def mezclar_elementos(entrada):
             c = c+1
 
     return nuevo_set
+
+def concatenar_lenguajes(lenguaje_1, lenguaje_2):
+    lenguaje_concatenado = set()
+    lenguaje_concatenado_list = list(lenguaje_concatenado)
+    lenguaje_1_lista = list(lenguaje_1)
+    lenguaje_2_lista = list(lenguaje_2)
+    i1 = 0
+    while i1 < len(lenguaje_1_lista):
+        i2 = 0
+        while i2< len(lenguaje_2_lista):
+            nuevo_elemento= lenguaje_1_lista[i1]+lenguaje_2_lista[i2]
+            lenguaje_concatenado_list.append(nuevo_elemento)
+            i2 = i2+1
+        i1 = i1+1
+    return lenguaje_concatenado_list
 
 def main():
     Alphabet1 = set(map(str, input("Ingrese la lista de simbolos de su primer alfabeto separados por espacios: ").split()))
@@ -30,6 +45,14 @@ def main():
     print(Alphabet3)
     Alphabet3 = Alphabet1 - Alphabet2
     print(Alphabet3)
-    print(mezclar_elementos(Alphabet1))
+    Alphabet4 = mezclar_elementos(Alphabet1)
+    print("lenguaje 1")
+    print(Alphabet4)
+    Alphabet5 = mezclar_elementos(Alphabet2)
+    print("lenguaje 2")
+    print(Alphabet5)
+    print("lenguaje concarena")
+    print(concatenar_lenguajes(Alphabet4, Alphabet5))
+    print(len(concatenar_lenguajes(Alphabet4, Alphabet5)))
 
 main()
